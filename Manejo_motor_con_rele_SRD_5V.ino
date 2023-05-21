f90191662747015ecb969785e2cf5a1b40f002e3
Bernardo-Tutoriales <134062247+Bernardo-Tutoriales@users.noreply.github.com>
2023-05-21 18:36:26 +0200

    Manejo de un rele SRD 5V
    Este archivo es para usar con Arduino IDE. Para ver el capítulo, búscalo en mi canal de YouTube 'Bernardo Tutoriales'

---
+++ Manejo_motor_con_rele_SRD_5V.ino
@@ -0,0 +1,38 @@
+/*
+Proyecto realizado por Bernardo Cruz.
+
+Canal de YouTube donde seguir el curso en el que realizamos
+todos los proyectos que puedas ver en mi página de TinkerCAD:
+Youtube:   http://www.youtube.com/c/BernardoVB
+
+Tienda de Amazon de mi canal en la que puedes adquirir
+cualquier artículo para tus proyectos de Arduino (No dejes de
+visitar el escaparate "ARDUINO, ROBÓTICA Y ELECTRÓNICA)":
+https://www.amazon.es/shop/bernardocruz
+
+Es muy aconsejable que veas el capítulo del ejercicio en el
+curso de Arduino de YouTube para una mejor comprension:
+
+________________________________
+*/
+
+void setup() {
+  //Declaramos el pin que activa y desactiva el relé
+  pinMode(10, OUTPUT);
+  //Abrimos puerto serie para ver el estado del relé
+  Serial.begin(9600);
+}
+
+void loop() {
+  //Activo relé y éste cierra el circuito y pone en marcha el motor
+  digitalWrite(10, HIGH);
+  Serial.println("Relé está activado y el motor funcionando");
+  //Mantengo el motor funcionando durante 3 segundos
+  delay(3000);
+
+  //Desactivo relé y éste abre el circuito y detiene el motor
+  digitalWrite(10, LOW);
+  Serial.println("Relé está desactivado y el motor detenido");
+  //Mantengo el motor parado durante 3 segundos
+  delay(3000);
+}
